30f76b54be84ba51d59c65b5a2913d68f7a0f294
PatriciaDivi <patriciavianad@gmail.com>
2021-12-10 22:44:48 -0300

    5 Requisito | Adicione um botão com id=criar-tarefa e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo | :rocket:

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
- 
+let tagInputText = document.querySelector('#texto-tarefa')
+let tagOlList = document.querySelector('#lista-tarefas')
+let tagButton = document.querySelector('#criar-tarefa')
+
+
+tagButton.addEventListener("click", mostrarNatela)
+
+function mostrarNatela(){
+    // console.log("entrei na function")
+    if (tagInputText.value === '') {
+        alert("Olá, Seja Bem-Vindo! Digite um texto")
+        return (false)
+    } else {
+
+        const createTagLi = document.createElement('li')
+        createTagLi.innerText = tagInputText.value
+
+        tagInputText.value = ""
+        tagOlList.appendChild(createTagLi)
+    }
+
+}
+mostrarNatela()
