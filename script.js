6f0db6a45915588e6352f3a5e3c76ab3fc5f052f
PatriciaDivi <patriciavianad@gmail.com>
2021-12-14 20:48:09 -0300

    7 Requisito| Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128) | CONCLUIDO | :rocket:

--- script.js
+++ script.js
@@ -3,21 +3,66 @@ let tagOlList = document.querySelector('#lista-tarefas')
 let tagButton = document.querySelector('#criar-tarefa')
 
 
-tagButton.addEventListener("click", mostrarNatela)
+
 
 function mostrarNatela(){
     // console.log("entrei na function")
-    if (tagInputText.value === '') {
-        alert("Olá, Seja Bem-Vindo! Digite um texto")
-        return (false)
-    } else {
+    tagButton.addEventListener("click", function() {
+        if (tagInputText.value === '') {
+            alert("Olá, Seja Bem-Vindo! Digite um texto")
+            return (false)
+        } else {
+    
+            const createTagLi = document.createElement('li')
+            createTagLi.classList.add('completed')
+            createTagLi.innerText = tagInputText.value
+    
+            tagInputText.value = ""
+            tagOlList.appendChild(createTagLi)
+        }
+    })
 
-        const createTagLi = document.createElement('li')
-        createTagLi.innerText = tagInputText.value
 
-        tagInputText.value = ""
-        tagOlList.appendChild(createTagLi)
+tagOlList.addEventListener("click", function(envet) {
+    console.log(tagOlList);
+    let listItems = document.querySelectorAll('li')
+
+    let colorWhite = "white"
+    let backgroundColorGray = "rgb(128, 128, 128)"
+
+    for (let positionNow = 0; positionNow < listItems.length; positionNow += 1) {
+
+        if (listItems[positionNow].style.backgroundColor === backgroundColorGray) {
+
+            envet.target.style.backgroundColor = colorWhite
+
+        } else {
+
+            envet.target.style.backgroundColor =  backgroundColorGray
+        }
     }
+})
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 mostrarNatela()
+
+
+
+
